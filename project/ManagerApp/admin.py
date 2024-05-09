c246960ab1787b363bba7fcaee3739bcf0534a62
Akashmj4532 <159401556+Akashmj4532@users.noreply.github.com>
2024-05-09 11:55:37 +0530

    project folder removed

--- project/ManagerApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Manager)
-admin.site.register(Packages)
-
-
-# Register your models here.
--- project/UserApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(User)
-admin.site.register(Booking)
-admin.site.register(Review)
-# Register your models here.
